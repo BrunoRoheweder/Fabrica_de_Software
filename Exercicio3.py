5054c727dce295634821880c6c7bb180035c8746
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-05-05 15:56:37 -0400

    Exercicio3 data 05/05

---
+++ Exercicio3.py
@@ -0,0 +1,87 @@
+a = 10 #a , b: esta sendo declarada que tem 10 e 20
+b = 20
+print(a)
+print(a + b) #(a + b): esta sendo somando e printado
+print (type(a)) #type: informa qual "função" ela é
+
+print ("\n") #Pular linha. Quebrar linha
+
+nome = input("Escreva seu nome: ") #input: Para escrever. input(): colocar algum texto dentro dos parenteses. nome: variavel que esta recebendo
+print (nome)
+
+print ("\n") #Pular linha. Quebrar linha
+
+print ("Digite seu nome: ")
+nome1 = input() #input: Para escrever
+print (nome1)
+
+print ("\n") #Pular linha. Quebrar linha
+
+idade = input("Digite sua idade: ")
+print(type(idade))
+
+print ("\n") #Pular linha. Quebrar linha
+
+salario = int(input("Salario? ")) #int: ja esta informando que tem que ser inteiro
+inposto = float(input("Digite o imposto: ")) #float: Ja esta informando que tem que ser float. float: Real
+
+print("\n") #Pular linha. Quebrar linha
+
+sal = int(input("Salario: "))
+print("O salario foi %s"%sal) #%s , %sal : %sal puxar para %s 
+
+print ("\n") #Pular linha. Quebrar linha
+
+a1 = 100
+b1 = 200.5
+c1 = "bruno"
+print ("o valor de a e %d, e o valor de b é %f"% (a1,b1))
+
+print ("\n") #Pular linha. Quebrar linha
+
+a2 = 100
+b2 = 200.5
+c2 = "bruno"
+print ("o valor de a e %d, e o valor de b é %.5f"% (a2,b2)) #%.5f quantidade de numeros ápos a virgula
+
+print ("\n") #Pular linha. Quebrar linha
+
+a3 = 100
+b3 = 200.5
+c3 = "bruno"
+print ("o valor de a e %d, e o valor de b é %.2f"% (a3,b3)) #%.2f quantidade de numeros ápos a virgula
+
+print ("\n") #Pular linha. Quebrar linha
+
+a4 = 100
+b4 = 200.5
+c4 = "bruno"
+print ("o valor de a e %d, e o valor de b é %.20f"% (a4,b4)) #%.20f quantidade de numeros ápos a virgula
+
+print ("\n") #Pular linha. Quebrar linha
+
+a5 = 100
+b5 = 200.5
+c5 = "bruno"
+print ("o valor de a e %d, e o valor de b é %.20f, e seu nome é %s"% (a5,b5,c5)) #%.20f quantidade de numeros ápos a virgula. %s: e string, ex: um texto
+
+print("\n") #Pular linha. Quebrar linha
+
+print("b","n","n",".",sep='a') #sep='a': vai subistituir a virgula 
+
+print("\n") #Pular linha. Quebrar linha
+
+nome2 = (input("Nome: "))
+sal1 = float(input("Salario: "))
+print ("O funcionario %s recebeu %.2f"
+%(nome2,sal1))
+
+print ("\n") #Pular linha. Quebrar linha
+
+a6 = "Ederson"
+b6 = "Costa"
+print("Prezado "+a6+" "+b6+"."+"Olá!") #+a6+ , +b6+: Operador de concatenação, mesma coisa aque uma virgula, so e usado com string
+
+print ("\n") #Pular linha. Quebrar linha
+
+
