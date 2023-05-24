920dc302f95d4f2f78ea2d61ea22a4efb64a8b20
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-05-24 17:38:34 -0400

    exercicio continuado no dia 24/05
    cadastro e matricula etc...

--- Exercicio12.py
+++ Exercicio12.py
@@ -22,13 +22,13 @@ lclasse = []
 lsexo = []
 lcor = []
 lmedia = []
-lcont = []
+
 cont = 0
 
 
 while True:
   
-    c = int(input("Digite 1 cadastro\nDigite 2 para consulta: "))
+    c = int(input("\nDigite 1 cadastro\nDigite 2 para consulta: "))
     if c == 1:
         nome = (input("Digite seu nome: "))
         lnome.append(nome)
@@ -79,18 +79,27 @@ while True:
         media = ((nota1+nota2+nota3+nota4)/4)
         lmedia.append(media)
         print(lmedia)
-        cont = cont + 1
-        lcont.append(cont)
-        c = int(input("Digite 1 para continuar\nDigite 2 para consulta\nDigite 0 para sair:  "))
+       
+        c = int(input("\nDigite 1 para continuar\nDigite 2 para consulta\nDigite 0 para sair:  "))
         
     elif c == 2:
         busca = int(input("Digite o numero da matricula: "))
-        print(lcont,lnome[busca-1],lsobrenome[busca-1],lidade[busca-1],lendereco[busca-1],lbairro[busca-1],lcidade[busca-1]\
-        ,lestado[busca-1],lpais[busca-1],lfone[busca-1],lcpf[busca-1],lpeso[busca-1],laltura[busca-1],lidade[busca-1]\
-        ,ln_do_cartao[busca-1],lemail[busca-1],lcep[busca-1],lnota1[busca-1],lnota2[busca-1],lnota3[busca-1],lnota4[busca-1]\
-        ,lserie[busca-1],lclasse[busca-1],lsexo[busca-1],lcor[busca-1],lmedia[busca-1])
+
+        print("\nNome:",lnome[busca-1] ,"\nSobrenome: ",lsobrenome[busca-1],"\nIdade: ",lidade[busca-1],"\nEndereço: ",lendereco[busca-1],"\nBairro: ",lbairro[busca-1],"\nCidade: ",lcidade[busca-1]\
+        ,"\nEstado: ",lestado[busca-1],"\nPaís: ",lpais[busca-1],"\nFone: ",lfone[busca-1],"\nCPF: ",lcpf[busca-1],"\nPeso: ",lpeso[busca-1],"\nAltura: ",laltura[busca-1]\
+        ,"\nNumero do cartão: ",ln_do_cartao[busca-1],"\nEmail: ",lemail[busca-1],"\nCEP: ",lcep[busca-1],"\nNota1: ",lnota1[busca-1],"\nNota2: ",lnota2[busca-1],"\nNota3: ",lnota3[busca-1],"\nNota4: ",lnota4[busca-1]\
+        ,"\nSerie: ",lserie[busca-1],"\nClasse: ",lclasse[busca-1],"\nSexo: ",lsexo[busca-1],"\nCor: ",lcor[busca-1],"\nMedia: ",lmedia[busca-1])
+
         c = int(input("Digite 1 para continuar o cadastro\nDigite 2 para continuar a consulta\nDigite 0 para sair: "))
-       
+    
+    elif c == 3:
+        delete = int(input("Digite a matricula a ser excluida: "))
+
+        del(lnome[delete-1] ,lsobrenome[delete-1],lidade[delete-1],lendereco[delete-1],lbairro[delete-1],\
+        lcidade[delete-1],lnome[delete-1],lpais[delete-1],lfone[delete-1],lcpf[delete-1],lpeso[delete-1],\
+        laltura[delete-1],ln_do_cartao[delete-1],lemail[delete-1],lcep[delete-1],lnota1[delete-1],lnota2[delete-1],\
+        lnota3[delete-1],lnota4[delete-1],lserie[delete-1],lclasse[delete-1],lsexo[delete-1],lcor[delete-1],lmedia[delete-1])
+
     elif c == 0:
         print("Obrigado volte sempre...")
         break
