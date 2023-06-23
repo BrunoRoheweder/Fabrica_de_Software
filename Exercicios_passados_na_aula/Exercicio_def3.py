a7a07d35790fbe945419c08c324c00e067d95366
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-06-23 15:52:48 -0400

    dia 23/06

---
+++ Exercicios_passados_na_aula/Exercicio_def3.py
@@ -0,0 +1,11 @@
+def contar(num):
+    
+    num = int(num)
+    for i in num:
+        cont = i + 1
+    print(f"O numero {numero} tem {cont} digitos.")
+
+numero = str(input("Digite um numero: "))
+contar(numero)
+
+
