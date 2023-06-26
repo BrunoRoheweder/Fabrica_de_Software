7755fa1ccd2ee03eeb0060128454005ead88e4aa
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-06-26 17:34:08 -0400

    dia 26/06/23
    bibliotecas

---
+++ Exercicios_passados_na_aula/Exercicio_com_três_pess.py
@@ -0,0 +1,60 @@
+import math
+import datetime as dt  # apelidando o nome da biblioteca como dt
+import datetime 
+from time import sleep
+import os
+import time
+
+# dmY = "Hoje e dia %d no mês %m e no ano de %Y"
+    
+# x = 3.5
+# math.ceil(x)  # ceil sempre retorna o valor maior do valor recebido, ex: entrada: 3.5, saida: 4
+# print(math.ceil(x))
+
+# a = -3
+# math.fabs(a)  # fabs sempre retorna o valor absoluto. ex: entrada: -3, saida: 3.0 
+# print(math.fabs(a))
+
+# b = 3
+# math.factorial(b)
+# print(math.factorial(b)) # factorial fatora o numero recebido. ex: entrada: 3, saida: 6
+
+# c = 3.6
+# math.floor(c)
+# print(math.floor(c))  # floor arredonda todos os numeros para baixo. ex: entrada: 3.5, saida: 3
+
+# d = 25
+# math.isqrt(d)
+# print(math.isqrt(d))  # isqrt retorna a raiz quadrada de um numero informado, em formato de inteiro. ex: entrada: 25, saida: 5
+
+# math.sqrt(d)
+# print(math.sqrt(d))   # isqrt retorna a raiz quadrada de um numero informado, em formato de float. ex: entrada: 25, saida: 5.0
+
+
+# f = 2
+# g = 10
+
+# math.pow(f, g)  # pow retorna potência. ex: entrada: 2^10, saida: 1024.0. obs: retorna float
+# print(math.pow(f, g))
+
+# print(datetime.date.today())  # datetime retorna o dia mes e ano
+# print(dt.date.today())  # datetime retorna o dia mes e ano, usando o apelido dt, nomeado no inicio do codigo
+
+# print(dt.date.today().strftime('%d/%m/%Y'))
+# print(dt.date.today().strftime(dmY))
+
+# print(dt.datetime.now())
+# while True:
+#     print(dt.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),flush=True)
+#     sleep(1.0)
+#     os.system('cls')
+#     break
+
+# q = 0
+# e = time.perf_counter()
+# while q < 10000:
+#     print(q)
+#     q += 1
+# w = time.perf_counter()
+# print(w-e)
+
