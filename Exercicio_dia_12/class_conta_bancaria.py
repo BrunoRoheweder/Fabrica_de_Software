889812c0a91993dcc8e1388c6e1bdfe06bab40cc
PaaottBruno <132005298+PaaottBruno@users.noreply.github.com>
2023-07-14 16:19:49 -0400

    update
    conta bancaria terminada

--- Exercicio_dia_12/class_conta_bancaria.py
+++ Exercicio_dia_12/class_conta_bancaria.py
@@ -1,6 +1,8 @@
+from pwinput import pwinput
+
 class Conta_Bancaria():
 
-    def __init__(self,nome,saldo,cpf,senha):
+    def __init__(self, nome, cpf, senha, saldo = 0):
 
         self.nome = nome
         self.__saldo = saldo
@@ -8,16 +10,31 @@ class Conta_Bancaria():
         self.__senha = senha
 
     def extrato(self,senha):
-        if senha == self.__senha:
-            print(f"Extrato\nNome: {self.nome}\nCPF: **********\nSaldo: *****\nSenha: **********")
+        try:
+            cpf = int(input("Digite seu cpf: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+        if senha == self.__senha and cpf == self.__cpf:
+            print(f"Extrato\nNome: {self.nome}\nCPF: {self.__cpf}\nSaldo: {self.__saldo}\n")
         else:
-            print("Senha Errada\nTente novamente.")
+            print("Senha errada ou cpf errados\nTente novamente.")
 
     def depositar(self,deposito):
-        self.deposito = deposito
-        self.__saldo = self.__saldo + self.deposito
+        self.__saldo += deposito
         print(f"O valor depositado foi {deposito}")
 
     def saque(self,sacar):
-        self.__saldo = self.__saldo - sacar
-        print(f"o valor sacado foi {sacar}")
+        self.__saldo -= sacar
+        try:
+            senha = int(pwinput("Digite sua senha: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+        if senha == self.__senha:
+            print("senha correta")
+
+        if self.__saldo > -1: 
+            print(f"o valor sacado foi {sacar}")
+
+        elif self.__saldo <= 0:
+            print("Saldo insuficiênte")
+            self.__saldo += sacar
--- Exercicio_dia_12/mainC.py
+++ Exercicio_dia_12/mainC.py
@@ -1,22 +1,63 @@
 from class_conta_bancaria import*
-
-nome = input("Digite seu nome: ")
-saldo = input("Digite seu saldo: ")
-cpf = input("Digite seu cpf: ")
-senha = input("Digite sua senha: ")
-
-c1 = Conta_Bancaria(nome, saldo, cpf, senha)
+from time import sleep
+from pwinput import pwinput
+import os
 
 while True:
-    op = input("1- cadastro\n2- saldo\n3- saque\n4- deposito\n0- Sair\n>>> ")
+    op = input("1- cadastro\n2- deposito\n3- saque\n4- extrato\n0- Sair\n>>> ")
 
     if op == "1":
-        c1.extrato()
+        os.system("cls")
+        nome = input("Digite seu nome: ")
+        try:
+            cpf = int(input("Digite seu cpf: "))
+            senha = int(pwinput("Digite sua senha: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+
+        c1 = Conta_Bancaria(nome, cpf, senha)
+        print(8*"=-","Cadastro realizado com sucesso","=-"*8)
+        os.system("pause")
+        os.system("cls")
 
     elif op == "2":
-        depositar = float(input("Digite a quantidade para depositar: "))
-        c1.depositar(depositar)
+        os.system("cls")
+        try:
+            deposito = float(input("Digite a quantidade para depositar: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+        c1.depositar(deposito)
+        os.system("pause")
+        os.system("cls")
     
     elif op == "3":
-        saque = float(input("Digite a quantidade para o saquq: "))
-        c1.saque(saque)
+        os.system("cls")
+        try:
+            sacar = float(input("Digite a quantidade para o saque: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+        c1.saque(sacar)
+        os.system("pause")
+        os.system("cls")
+
+    elif op == "4":
+        os.system("cls")
+        try:
+            senha = int(pwinput("Digite seua senha: "))
+        except ValueError:
+            print("Use apenas numeros!!")
+        c1.extrato(senha)
+        os.system("pause")
+        os.system("cls")
+
+    elif op == "0":
+        cont = 0
+        os.system("cls")
+        print("Saindo...")
+        while cont <=2 :
+            cont+=1
+            print(f"{cont} ",end="",flush=True)
+            sleep(0.8)
+        os.system("cls")
+        print("\nAté logo\n")
+        break
