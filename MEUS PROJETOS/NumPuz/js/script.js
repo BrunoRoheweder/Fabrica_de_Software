062507e94dbbadc72b586e2edf61a8d7d8980ead
Javador48 <paaott08@gmail.com>
2024-01-26 00:31:45 -0400

    terminar depois
    estou fazendo um jogo chamado numpuz

---
+++ MEUS PROJETOS/NumPuz/js/script.js
@@ -0,0 +1,13 @@
+function alertar(){
+    alert("O botão foi clicado!");
+}
+
+var array = [[1],[2],[3],
+             [4],[5],[6],
+             [7],[8],[9]];
+
+function mover(num){
+    for (num; num < array[8][0];num++){
+        alert("deu certo, ou quase");
+    }
+}
