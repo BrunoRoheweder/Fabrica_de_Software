894edcc14b48a44f8862575ccbf552f4578f7f8f
Javador48 <paaott08@gmail.com>
2024-01-31 23:50:00 -0400

    Jogo concluido
    Jogo numpuz concluido, agora so estilizar

--- MEUS PROJETOS/NumPuz/js/script.js
+++ MEUS PROJETOS/NumPuz/js/script.js
@@ -60,214 +60,105 @@ var numerosSorteados = sortearNumeros(8, 1, 8);
 
 var array = [[num1],[num2],[num3],
              [num4],[num5],[num6],
-             [num7],[num8],[num0]];
-
+             [num7],[num8],[num0]];     
 
 numerosSorteados.forEach(function(valor, index) {
     array[index][0] = valor;
-    array[8][0] = 0;
-    // array[6][0] = 0;
+    array[8][0] = parseInt(0);
     // console.log(array[0][0]);
 });
+
+var numero1 = document.getElementById("num1");
+var numero_1 = numero1.innerHTML = array[0][0];
+
+var numero2 = document.getElementById("num2");
+var numero_2 = numero2.innerHTML = array[1][0];
+
+var numero3 = document.getElementById("num3");
+var numero_3 = numero3.innerHTML = array[2][0];
+
+var numero4 = document.getElementById("num4");
+var numero_4 = numero4.innerHTML = array[3][0];
+
+var numero5 = document.getElementById("num5");
+var numero_5 = numero5.innerHTML = array[4][0];
+
+var numero6 = document.getElementById("num6");
+var numero_6 = numero6.innerHTML = array[5][0];
+
+var numero7 = document.getElementById("num7");
+var numero_7 = numero7.innerHTML = array[6][0];
+
+var numero8 = document.getElementById("num8");
+var numero_8 = numero8.innerHTML = array[7][0];
+
 
 // Fim da adicão dos numeros
 
 
-
-
 // Verificando se pode se mover e mover
 
-// var array = [[num1],[num2],[num3],[num4],[num5],[num6],[num7],[num8],[num0]];
-//      index =   0   ,  1   ,  2   ,  3   ,  4   ,  5   ,  6   ,  7   ,  8   .
+function mover(origem, destino) {
+    if (array[destino][0] === 0) {
+        // Condição de movimento
+        var numeroOrigem = array[origem][0];
+        array[origem][0] = 0;
+        array[destino][0] = numeroOrigem;
 
-// var array = [[num1],[num2],[num3],
-//              [num4],[num5],[num6],
-//              [num7],[num8],[num0]];
-//      index =   0   ,  1   ,  2   ,
-//                3   ,  4   ,  5   ,
-//                6   ,  7   ,  8   .
-//      Verificar um index anterior e um posterior e o terceiro index apos o seu proprio index
+        var numeroOrigemElement = document.getElementById("num" + (origem + 1));
+        numeroOrigemElement.innerHTML = " ";
+
+        var numeroDestinoElement = document.getElementById("num" + (destino + 1));
+        numeroDestinoElement.innerHTML = numeroOrigem;
+    }
+}
 
 function mover_1() {
-    array.forEach(function(valor, index) {
-        if (index == 0) { // index 0 numero 1
-            if (array[1][0] == 0) {
-                // Condição de movimento
-            }
-
-            else if (array[3][0] == 0) {
-                // Condição de movimento
-            }
-        }
-    })
+    mover(0, 1);
+    mover(0, 3);
 }
 
 function mover_2() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 1) { // index 1 numero 2
-            if (array[0][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[2][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[4][0] == 0) {
-                // Condição de movimento
-            }
-        }
-    })
+    mover(1, 0);
+    mover(1, 2);
+    mover(1, 4);
 }
-
 function mover_3() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 2) { // index 2 numero 3
-            if (array[1][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[5][0] == 0) {
-                // Condição de movimento
-            }
-        }
-    })
+    mover(2, 1);
+    mover(2, 5);
 }
 
 function mover_4() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 3) { // index 3 numero 4
-            if (array[0][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[4][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[6][0] == 0) {
-                // Condição de movimento
-            }
-        }
-    })
+    mover(3, 0);
+    mover(3, 4);
+    mover(3, 6);
 }
-
 function mover_5() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 4) { // index 4 numero 5
-            if (array[1][0] == 0) {
-            // Condição de movimento
-            }
-            else if (array[3][0] == 0) {
-            // Condição de movimento
-            }
-            else if (array[5][0] == 0) {
-            // Condição de movimento
-            }
-            else if (array[7][0] == 0) {
-            // Condição de movimento
-            }
-        }
-    })
+    mover(4, 1);
+    mover(4, 3);
+    mover(4, 5);
+    mover(4, 7);
 }
 
 function mover_6() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 5) { // index 5 numero 6
-            if (array[2][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[4][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[8][0] == 0) {
-                // Condição de movimento
-                var numero8 = document.getElementById("num6");
-                numero8.innerHTML = " "
-
-                var numero9 = document.getElementById("num9");
-                numero9.innerHTML = "6"
-            }
-        }
-    })
+    mover(5, 2);
+    mover(5, 4);
+    mover(5, 8);
 }
-
 function mover_7() {
-    array.forEach(function(valor, index) { 
-
-        if (index == 6) { // index 6 numero 7
-            if (array[3][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[7][0] == 0) {
-                // Condição de movimento
-                array[7][0] = array[6][0]
-                array[6][0] = 0
-
-                var numero8 = document.getElementById("num8");
-                numero8.innerHTML = "8"
-
-                var numero7 = document.getElementById("num7");
-                numero7.innerHTML = " "
-            }
-        }
-    })
+    mover(6, 3);
+    mover(6, 7);
 }
 
 function mover_8() {
-    array.forEach(function(valor, index) {
-        
-        if (index == 7) { // index 7 numero 8
-            if (array[4][0] == 0) {
-                // Condição de movimento
-            }
-            else if (array[6][0] == 0) {
-                // Condição de movimento
-                array[6][0] = array[7][0]
-                array[7][0] = 0
-
-                var numero7 = document.getElementById("num7");
-                numero7.innerHTML = "8"
-
-                var numero8 = document.getElementById("num8");
-                numero8.innerHTML = " "
-            }
-            else if (array[8][0] == 0) {
-                // Condição de movimento
-
-                array[8][0] = array[7][0]
-                array[7][0] = 0
-
-                var numero8 = document.getElementById("num8");
-                numero8.innerHTML = " "
-
-                var numero9 = document.getElementById("num9");
-                numero9.innerHTML = "8"
-            }
-        }
-    })
+    mover(7, 4);
+    mover(7, 6);
+    mover(7, 8);
 }
 
 function mover_9() {
-    array.forEach(function(valor, index) {
-
-        if (index == 8) { // index 6 numero 7
-            if (array[7][0] == 0) {
-                // Condição de movimento
-                array[7][0] = array[8][0]
-                array[8][0] = 0
-
-                var numero9 = document.getElementById("num9");
-                numero9.innerHTML = " "
-
-                var numero8 = document.getElementById("num8");
-                numero8.innerHTML = "8"
-            }
-            else if (array[5][0] == 0) {
-                // Condição de movimento
-            }
-        }
-    })
+    mover(8, 5);
+    mover(8, 7);
 }
 
 // Fim da vereficação de mover e mover
@@ -275,22 +166,31 @@ function mover_9() {
 
 //  CONFIRMANDO QUE TODOS ESTÃO IGUAIS OU SEJA TODOS ESTÃO NOS DEVIDOS LUGARES
 //  VERIFICAÇÃO 
+function verificar() {
+    var array_confirm = [[1],[2],[3],
+                        [4],[5],[6],
+                        [7],[8],[0]];
 
-var array_confirm = [[1],[2],[3],
-                     [4],[5],[6],
-                     [7],[8],[0]];
+    var todosConfirmados = true;  
 
-var todosConfirmados = true;  
+    array_confirm.forEach(function(valor, index) {
+        if (array_confirm[index][0] !== array[index][0]) {
+            todosConfirmados = false;
+        }
+    });
 
-array_confirm.forEach(function(valor, index) {
-    if (array_confirm[index][0] !== array[index][0]) {
-        todosConfirmados = false;
+    if (todosConfirmados) {
+        alert("Você Ganhou");
+        var confirmar = confirm("Jogar novamente")
+        if (confirmar) {
+            location.reload();
+        }
     }
-});
-
-if (todosConfirmados) {
-    console.log("Você Ganhou");
-} else {
-    console.log("Pelo menos um número não está confirmado");
+    
+    // else {
+        //     console.log("Pelo menos um número não está confirmado");
+        // }
+        //  FIM DA VERIFICAÇÃO  
 }
-//  FIM DA VERIFICAÇÃO  
+
+document.addEventListener("click", verificar)
